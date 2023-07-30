16047242a47766cd793c70f8fd1c48f800148931
Bimo <sani21bimo@gmail.com>
2023-07-30 12:23:47 +0700

    modify nodemon again

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.env.MODE_ENV !== 'production'?'localhost':'0.0.0.0',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes:{
       cors:{
         origin:['*'], //* = allow access to all dir
